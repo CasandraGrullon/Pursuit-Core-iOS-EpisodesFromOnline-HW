0be9f1a93ef7acbe297b79dab472f503a6186d38
Casandra <casandragrullon@pursuit.org>
2019-12-10 16:38:58 -0500

    created episodeApiclient

---
+++ Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/Network Services/EpisodeAPIClient.swift
@@ -0,0 +1,36 @@
+//
+//  EpisodeAPIClient.swift
+//  Pursuit-Core-iOS-Episodes-from-Online
+//
+//  Created by casandra grullon on 12/10/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import Foundation
+
+struct EpisodeAPIClient{
+    static func getEpisodes(for showId: Int, completion: @escaping(Result<Episode, AppError>) -> ()){
+        let endpointURL = "http://api.tvmaze.com/shows/\(showId)/episodes"
+        
+        guard let url = URL(string: endpointURL) else {
+            completion(.failure(.badURL(endpointURL)))
+            return
+        }
+        let request = URLRequest(url: url)
+        
+        NetworkHelper.shared.performDataTask(with: request) { (result) in
+            switch result {
+            case .failure(let appError):
+                completion(.failure(.networkClientError(appError)))
+            case .success(let data):
+                do{
+                    let searchResults = try JSONDecoder().decode(Episode.self, from: data)
+                    let tvEpisodes = searchResults
+                    completion(.success(tvEpisodes))
+                }catch{
+                    completion(.failure(.decodingError(error)))
+                }
+            }
+        }
+    }
+}
